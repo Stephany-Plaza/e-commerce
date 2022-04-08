bd11e5948d9bed2f976aa0da04d9cd88f14a5676
Stephany Plaza <splaza410@gmail.com>
2022-04-08 00:49:32 -0400

    eliminacion de archivos

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -11,16 +11,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_appId
 };
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyDxGQg824s_jS_6iw3zgZbzDUS9Xxd4KYg",
-  authDomain: "proyecto-react-1-f8902.firebaseapp.com",
-  projectId: "proyecto-react-1-f8902",
-  storageBucket: "proyecto-react-1-f8902.appspot.com",
-  messagingSenderId: "410292745929",
-  appId: "1:410292745929:web:bb8fed8e08e30462e6ff18"
-};*/
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const firestoreDb = getFirestore(app)
 
